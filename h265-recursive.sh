2825576bc064db42eba5a6e2866b1db1c9a25d98
ijortengab <m_roji28@yahoo.com>
2021-10-27 23:31:48 +0700

    Improve h265-recursive.sh faster.

--- h265-recursive.sh
+++ h265-recursive.sh
@@ -2,10 +2,16 @@
 
 command -v "h265.sh" >/dev/null || { echo "h265.sh command not found."; exit 1; }
 
-find . -type d \
+pwd="$PWD"
+
+find * -type d \
     ! -path '*h264_existing*' \
     ! -path '*h265_existing*' \
     ! -path '*h265_converted*' \
     ! -path '*mp4_unknown*' \
     ! -path '*h265_log*' \
-    -exec sh -c 'echo "$0"; cd "$0"; h265.sh' {} \;
+    | while IFS= read -r path; do
+    path="${pwd}/${path}";
+    echo -ne "\e[35m" >&2; echo -n "cd '$path'"; echo -ne "\e[0m" >&2; echo
+    cd "$path"; h265.sh
+done
